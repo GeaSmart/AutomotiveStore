13c8c7d02183a19f6dcbc65dc2f824d551d526ef
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-07 15:26:42 -0500

    creating applicationdbcontext

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+    }
+}
