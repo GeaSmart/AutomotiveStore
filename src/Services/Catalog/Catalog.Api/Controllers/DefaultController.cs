05fb7a729f4aa5c840e757e22a9a566b4f60b821
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-07 12:12:28 -0500

    creating project for catalog

---
+++ src/Services/Catalog/Catalog.Api/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Running...";
+        }
+    }
+}
