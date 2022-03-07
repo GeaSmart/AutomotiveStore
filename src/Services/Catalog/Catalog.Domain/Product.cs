3543343a9ea3366e5ea48ba54158c86bc7fab109
Gerson Azabache <gerson_dunk@hotmail.com>
2022-03-07 12:57:14 -0500

    creating project for models (entities)

---
+++ src/Services/Catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        //Propiedades de navegación
+        public ProductInStock Stock { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Domain/ProductInStock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain
+{
+    public class ProductInStock
+    {
+        public int ProductInStockId { get; set; }
+        public int Stock { get; set; }
+        public int ProductId { get; set; }
+    }
+}
